40244f33251738299b747399fde8ac8b735a587b
eJwtzMkOgjAUheF36ZoFqKHQndGlQ4xDdNV0uGBDofUWRGN8dxvD7uQk3/8hQikI4eQa6AgjVFe0zGUOVMyVni8gzbSgqkgLpXRJMymLvIBKk4R4dE+jASf6mUq8n1L13fHnYz+2h2O4bs63u8xW23pxoVKul7PZfpdmh1sJZUz9Ce/fHqKTIBAwvkG5/yF0azrmB2mN4g28EwShmcM6Du+SIQAyaIWxyeiwqawbI4aXNwiBi56wbrD2+/0ByzdQYQ== <mustfafadhel@gmail.com>
2023-01-11 23:32:27 +0300

    COMMIT

---
+++ flutter_service_worker.js
@@ -0,0 +1,308 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "4bffdbbc6826f91cc8e7c7803cd73f66",
+"assets/FontManifest.json": "a448e41aeecd2813eca41a8c25ad3126",
+"assets/fonts/Hacen_Tunisia_Bold.ttf": "fe04d1be46f7a04bada6f87558801376",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/image/-01.png": "c7e0e280a8ef5bd4d82c742908e6f701",
+"assets/image/1.jpg": "5220140e96fd6f70d7f5424026acd60d",
+"assets/image/2.jpg": "f0f92699f3cbc6098eea6885fe4bdcb2",
+"assets/image/3.jpg": "2a8c99c3b1ccb074cedb69ee494fd7f3",
+"assets/image/93462-login.gif": "3943edc8b9c78d3564cc76668ba5ea3a",
+"assets/image/dis.png": "38f20159501563add70691c67ae3c8b5",
+"assets/image/done.png": "05ccfc88cc3c2c9b01246e9ff0736d6d",
+"assets/image/go.png": "8d8e2d82f54ae1977b1a7dff1dbd187a",
+"assets/image/index.png": "cd8caecbb04162cbcdde4e910d68b888",
+"assets/image/lo.gif": "486c32423cde4b74ed4e2762d9358821",
+"assets/image/lock.png": "bf888471c70f24e74c49155df3a6d2d3",
+"assets/image/log.png": "023fbec8a9a67ab3df2cfd6db0d31539",
+"assets/image/map.png": "79aaad2aa2083052e6d9b475b3ab5c74",
+"assets/image/mo1.png": "a2074994c2e3fc281f3d1b183c6b0a48",
+"assets/image/mo2.png": "9b8532919b09a726c975bbeb15158630",
+"assets/image/mu1.jpg": "972d405d9828413e25e3e0f9daf0c6c2",
+"assets/image/mu10.jpg": "0ea76aa53e6b0754e9a1dbdad338d521",
+"assets/image/mu2.jpg": "60603a442edc27f5080b0c4dfb0cf939",
+"assets/image/mu3.jpg": "3d6872c38b051254c733d58607b745f0",
+"assets/image/mu4.jpg": "1c700a077ab0abe3200200c5e2314519",
+"assets/image/mu5.jpg": "be666caf0e6d15a1dab22bb442b14493",
+"assets/image/mu6.jpg": "3cbf7ecd88651c72724863e602b25ef1",
+"assets/image/mu7.jpg": "ccbc02e29bc1d053b2b20d499ead6524",
+"assets/image/mu8.jpg": "eb497deb79b2811911e66e6d77c1c6d0",
+"assets/image/mu9.jpg": "149ff42246863d10074263c4d1f852cf",
+"assets/image/person.gif": "54388337af057ecf76d01974167d299c",
+"assets/image/s.jpg": "4c2ac0d2dfc4d9348765c889fc159f8e",
+"assets/image/tam.jpg": "4e9f4c68dd9d90c03d58c0c2577e88a7",
+"assets/image/user.png": "d991f2f5126d4ae3ceff87a8783f088e",
+"assets/image/w.png": "4437cdf9472eda8b2f0e95bc723e6b18",
+"assets/image/wait.gif": "740b22c35e679107d97eac9c2e60c34e",
+"assets/image/who.png": "282ed0b59f0098da36e24cf04d6acb4e",
+"assets/image/who2.png": "89bce25564e48937477247f398d23a5b",
+"assets/NOTICES": "d89b4301091c4c807ed9f1b0825a3b46",
+"assets/packages/awesome_dialog/assets/flare/error.flr": "e3b124665e57682dab45f4ee8a16b3c9",
+"assets/packages/awesome_dialog/assets/flare/info.flr": "bc654ba9a96055d7309f0922746fe7a7",
+"assets/packages/awesome_dialog/assets/flare/info2.flr": "21af33cb65751b76639d98e106835cfb",
+"assets/packages/awesome_dialog/assets/flare/info_without_loop.flr": "cf106e19d7dee9846bbc1ac29296a43f",
+"assets/packages/awesome_dialog/assets/flare/question.flr": "1c31ec57688a19de5899338f898290f0",
+"assets/packages/awesome_dialog/assets/flare/succes.flr": "ebae20460b624d738bb48269fb492edf",
+"assets/packages/awesome_dialog/assets/flare/succes_without_loop.flr": "3d8b3b3552370677bf3fb55d0d56a152",
+"assets/packages/awesome_dialog/assets/flare/warning.flr": "68898234dacef62093ae95ff4772509b",
+"assets/packages/awesome_dialog/assets/flare/warning_without_loop.flr": "c84f528c7e7afe91a929898988012291",
+"assets/packages/awesome_dialog/assets/rive/error.riv": "e74e21f8b53de4b541dd037c667027c1",
+"assets/packages/awesome_dialog/assets/rive/info.riv": "2a425920b11404228f613bc51b30b2fb",
+"assets/packages/awesome_dialog/assets/rive/info_reverse.riv": "c6e814d66c0e469f1574a2f171a13a76",
+"assets/packages/awesome_dialog/assets/rive/question.riv": "00f02da4d08c2960079d4cd8211c930c",
+"assets/packages/awesome_dialog/assets/rive/success.riv": "73618ab4166b406e130c2042dc595f42",
+"assets/packages/awesome_dialog/assets/rive/warning.riv": "0becf971559a68f9a74c8f0c6e0f8335",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/packages/fancy_dialog/assets/gif1.gif": "c5738331b11f3db2db8ad0b3776966a7",
+"assets/packages/fancy_dialog/assets/gif2.gif": "10511cc7d2aebde489ded7ecd56344b8",
+"assets/packages/fancy_dialog/assets/gif3.gif": "9756c2a05e2dd85309fe4b3bc5d62357",
+"assets/packages/fancy_dialog/assets/gif4.gif": "1785ef046094bead949e9afc4ad1445a",
+"assets/packages/fancy_dialog/assets/gif5.gif": "98be0097a3eac5fb4e5c76fb271caaf1",
+"assets/packages/fancy_dialog/assets/gif6.gif": "da048fce2f9995f25fce360ec511567d",
+"assets/packages/google_fonts_arabic/lib/fonts/Amiri/Amiri-Bold.ttf": "2accfcd6b3faf83d349942a19b91e12b",
+"assets/packages/google_fonts_arabic/lib/fonts/Amiri/Amiri-BoldItalic.ttf": "1f0cfd59bef4321ff35af3ea69415e55",
+"assets/packages/google_fonts_arabic/lib/fonts/Amiri/Amiri-Italic.ttf": "ca54f2483d9429c0a47fba80249ceede",
+"assets/packages/google_fonts_arabic/lib/fonts/Amiri/Amiri-Regular.ttf": "a61fbc4d3da365e17f68e1bba6415e47",
+"assets/packages/google_fonts_arabic/lib/fonts/Aref_Ruqaa/ArefRuqaa-Bold.ttf": "7b87d843f32f71e0cf71766945d712e4",
+"assets/packages/google_fonts_arabic/lib/fonts/Aref_Ruqaa/ArefRuqaa-Regular.ttf": "8ff92bc4b7f51b61d2d628220a08d2d3",
+"assets/packages/google_fonts_arabic/lib/fonts/Baloo_Bhaijaan/BalooBhaijaan-Regular.ttf": "28190ae5cd54e8bf270404320d5e0821",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-Black.ttf": "41774c33b29f951d86110f0e8cd527bb",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-Bold.ttf": "ca9036ba6a756880f334d959b1b5f549",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-ExtraLight.ttf": "118c56fd3f0773d43bdcd16deb8370d0",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-Light.ttf": "532ee41c709ac28455bed88dd839ddb8",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-Regular.ttf": "8e62cfbb90ccadc00b59b977c93eb31a",
+"assets/packages/google_fonts_arabic/lib/fonts/Cairo/Cairo-SemiBold.ttf": "984ec9c232c5936cadb14e87cf1283db",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-Bold.ttf": "919bffaf44e0410995355e7441435370",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-ExtraBold.ttf": "73fcacc8fd46a0eed3037a20982d2cc7",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-ExtraLight.ttf": "0eccd5677b2ef5b0342169564e571a05",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-Light.ttf": "e2f7d3402c1c52456c09db40a80228f0",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-Medium.ttf": "c3af486a73a7461b1122665df86a7733",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-Regular.ttf": "7a53368c4704181a547f440c6273159b",
+"assets/packages/google_fonts_arabic/lib/fonts/Changa/Changa-SemiBold.ttf": "125489c0710514546290914ac822a720",
+"assets/packages/google_fonts_arabic/lib/fonts/El_Messiri/ElMessiri-Bold.ttf": "1a4d3ffd4a8a7017d2a57c22105dead4",
+"assets/packages/google_fonts_arabic/lib/fonts/El_Messiri/ElMessiri-Medium.ttf": "a88b6e918867744f5a28a5f4646c712f",
+"assets/packages/google_fonts_arabic/lib/fonts/El_Messiri/ElMessiri-Regular.ttf": "f987603b0ad311424b5c64c55e10d10c",
+"assets/packages/google_fonts_arabic/lib/fonts/El_Messiri/ElMessiri-SemiBold.ttf": "a2fc08a54a46b0663a084ce4ba19cce2",
+"assets/packages/google_fonts_arabic/lib/fonts/Harmattan/Harmattan-Regular.ttf": "bcd87a685fc9fa2f88ae49bf9662649c",
+"assets/packages/google_fonts_arabic/lib/fonts/Jomhuria/Jomhuria-Regular.ttf": "7d6b466d0e08fd984705b6190e6554a6",
+"assets/packages/google_fonts_arabic/lib/fonts/Katibeh/Katibeh-Regular.ttf": "b9a90c628ecd4066bc9a44f548b67ff9",
+"assets/packages/google_fonts_arabic/lib/fonts/Lalezar/Lalezar-Regular.ttf": "c07a18bb821945af6ec7de632e877731",
+"assets/packages/google_fonts_arabic/lib/fonts/Lateef/LateefRegOT.ttf": "f98cf82fba21f78b335a41f343c5f0c9",
+"assets/packages/google_fonts_arabic/lib/fonts/Lemonada/Lemonada-Bold.ttf": "ec008f8a072ecd3d535f2cac97e9ba89",
+"assets/packages/google_fonts_arabic/lib/fonts/Lemonada/Lemonada-Light.ttf": "f6dbfb6fa1cf1d1a3070c0f0ceb2d585",
+"assets/packages/google_fonts_arabic/lib/fonts/Lemonada/Lemonada-Regular.ttf": "659eae40390059a683cc91faf4df4ca9",
+"assets/packages/google_fonts_arabic/lib/fonts/Lemonada/Lemonada-SemiBold.ttf": "91d517aec1170078f0b2eab23b4eef26",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-Black.ttf": "77e982cb003544f8c4832af58f5a8051",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-Bold.ttf": "6087ba6d5c2ac2ad094c8a545839775e",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-ExtraLight.ttf": "b5043d5fc92c52c730e4faff41118815",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-Light.ttf": "7b3ad1c8f7d7dbc35890123434adae55",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-Medium.ttf": "e3d9e7676a26eae957fad1e69c33d5a7",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-Regular.ttf": "9b3ddca6af7328102938afab0d55bc9d",
+"assets/packages/google_fonts_arabic/lib/fonts/Mada/Mada-SemiBold.ttf": "28191d02304034b2421f2bacb91b78aa",
+"assets/packages/google_fonts_arabic/lib/fonts/Markazi_Text/MarkaziText-Regular.ttf": "367d77a763df54f70c6ebe345598f2fb",
+"assets/packages/google_fonts_arabic/lib/fonts/Mirza/Mirza-Bold.ttf": "d3cc57b384c4ccfcab258d4ade64b7f5",
+"assets/packages/google_fonts_arabic/lib/fonts/Mirza/Mirza-Medium.ttf": "3b667481a859085d1a27e221bf45f3a9",
+"assets/packages/google_fonts_arabic/lib/fonts/Mirza/Mirza-Regular.ttf": "21fd4e3c1c6f103d8b00b8a30d211c49",
+"assets/packages/google_fonts_arabic/lib/fonts/Mirza/Mirza-SemiBold.ttf": "27b1558b9550a3196031a61fc6e0687e",
+"assets/packages/google_fonts_arabic/lib/fonts/Rakkas/Rakkas-Regular.ttf": "1197ba69414123d92777f819a43a7d27",
+"assets/packages/google_fonts_arabic/lib/fonts/Reem_Kufi/ReemKufi-Regular.ttf": "1cbfee67b7f1e63e1334e8800d5450b6",
+"assets/packages/google_fonts_arabic/lib/fonts/Scheherazade/Scheherazade-Bold.ttf": "b26c8ba6aae29ef98d54c0841398ef34",
+"assets/packages/google_fonts_arabic/lib/fonts/Scheherazade/Scheherazade-Regular.ttf": "87ffd3a053cd6c09186452cb10d9a15a",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-Black.ttf": "d8e4db47417985783913a2d64ca6e8cf",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-Bold.ttf": "73222b42f57d11db8ed71c1752e121c0",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-ExtraBold.ttf": "87a0f0773f0cdc28fd8dd3603bf34837",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-ExtraLight.ttf": "4dba176f2d532f19d9c4c61502bca07b",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-Light.ttf": "5c8f739351e568ec0c9fc0dc8a6994b4",
+"assets/packages/google_fonts_arabic/lib/fonts/Tajawal/Tajawal-Medium.ttf": "1472d65abf09fa765956fd3d32dadf48",
+"assets/shaders/ink_sparkle.frag": "dabaa63229e255a01a560b5cad18a0df",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"index.html": "1708aee2116c68f82255c1b4717618a6",
+"/": "1708aee2116c68f82255c1b4717618a6",
+"main.dart.js": "84f4f60137893d33038bbb1a0eb2c450",
+"manifest.json": "c9c6e178b1a37f4ddb356807fa7235ed",
+"version.json": "322dfa94430cc69aa45ce83495f46466"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"index.html",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
